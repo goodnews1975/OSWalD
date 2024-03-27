26e297b0712b7a9ae79d96003c588a0867ac9ae1
Thomas Laur <thomas.laur75@googlemail.com>
2024-03-27 21:05:48 +0100

    adding switch

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ Main Program
 
 from machine import Pin, I2C
 from ssd1306 import SSD1306_I2C
-from picozero import Button,LED
+from picozero import Button,LED, pico_led
 import framebuf
 import time
 
@@ -23,8 +23,21 @@ def display_clear():
     display.show()    
 # End initializing display
 
+# Initialisierung von GPIO0 als Eingang
+btn = Pin(0, Pin.IN, Pin.PULL_DOWN)
+
+# Initialisierung von GPIO25 als Ausgang
+led_onboard = Pin(25, Pin.OUT)
+
 
 display.text("Starting up....",0,0)
 display.show()
 time.sleep(2)
 display_clear()
+
+# Funktion zur Taster-Auswertung
+while True:
+    if btn.value() == 0:
+        led_onboard.on()
+    else:
+        led_onboard.off()
